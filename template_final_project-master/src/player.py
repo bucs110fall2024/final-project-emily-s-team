0ab384aa6de2f2e35eb97f623d64379542e8857e
Emily Scott <escott4@binghamton.edu>
2024-11-25 17:12:18 -0500

    sss

--- template_final_project-master/src/player.py
+++ template_final_project-master/src/player.py
@@ -1,22 +1,36 @@
 
 import pygame
-class Cloud(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
   
-  def __init__(self,name):
+  def __init__(self,x,y,img_file):
     #setup pygame data
     super().__init__()
-    self.name
-    self.size = "small"
-    self.image = pygame.image.load(f"assets/{name}.png")
-    self.rect = self.image.get_rect()
-    self.rect.x = 0
-    self.rect.y = 0
-    is_jumping = None
+    # Load the image for the player
+        self.image = pygame.image.load(img_file).convert_alpha()
+        
+        # Get the rectangle of the image for positioning
+        self.rect = self.image.get_rect()
+        
+        # Set the initial position of the player
+        self.rect.topleft = (x, y)
 
-  def jump(self)
-    if is_jumping:
-      pass
-  def update(self):
-    self.rect.x -= 2
+        # Movement attributes
+        self.speed = 5
+
+  def move(self, keys):
+        
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+
+    def update(self, keys):
+       
+        self.move(keys)
+  
 
    
