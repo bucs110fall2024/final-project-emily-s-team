d4f90ebaa4f4c81d5ae48a1ec39b9edda4878e8a
Emily Scott <escott4@binghamton.edu>
2024-12-02 20:26:32 -0500

    ssss

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -1,26 +1,42 @@
-
 import pygame
-from src import player
+from src.player import Player
+
 class Controller:
-  
-  def __init__(self):
-    #setup pygame data
-    pygame.init()
-    pygame.event.pump()
-  def mainloop(self):
+    def __init__(self):
+        # Initialize controller variables
+        self.screen = None
+        self.clock = None
+        self.running = True
+        self.state = "menu"
+
+    def mainloop(self):
+        # Setup screen and clock
         self.screen = pygame.display.set_mode((800, 600))
-
-
-        # Clock for controlling frame rate
         self.clock = pygame.time.Clock()
 
         # Initialize player and sprite groups
-        
+        player = Player(375, 275)  # Starting position near the center
+        all_sprites = pygame.sprite.Group()  # Create the sprite group
+        all_sprites.add(player)
 
-        # State variable
-        self.running = True
-        self.state = "menu"
+        while self.running:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # Get pressed keys
+            keys = pygame.key.get_pressed()
+
+            # Update sprites
+            all_sprites.update(keys)
+
+            # Render the screen
+            self.screen.fill((0, 0, 0))  # Clear the screen with black
+            all_sprites.draw(self.screen)  # Draw all sprites
+            pygame.display.flip()
+
+            # Control frame rate
+            self.clock.tick(60)
+
         pygame.quit()
-  ### below are some sample loop states ###
-
-
--- template_final_project-master/src/player.py
+++ template_final_project-master/src/player.py
@@ -1,36 +1,30 @@
-
 import pygame
+
 class Player(pygame.sprite.Sprite):
-  
-  def __init__(self,x,y):
-    #setup pygame data
-    super().__init__()
-    # Load the image for the player
-    self.rect1 = pygame.Rect(50, 50, 100, 100)
-    
-        
-        # Get the rectangle of the image for positioning
-   
-        
-        # Set the initial position of the player
-    self.rect1.topleft = (x, y)
+    def __init__(self, x, y):
+        super().__init__()
+        # Create a surface for the player (50x50 red rectangle as a placeholder)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 0, 0))  # Fill with red color
+
+        # Create a rectangle for positioning and collision
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
         # Movement attributes
-    self.speed = 5
+        self.speed = 5
 
-  def move(self, keys):
-    if keys[pygame.K_UP]:
-        self.rect1.y -= self.speed
-    if keys[pygame.K_DOWN]:
-        self.rect1.y += self.speed
-    if keys[pygame.K_LEFT]:
-        self.rect1.x -= self.speed
-    if keys[pygame.K_RIGHT]:
-        self.rect1.x += self.speed
+    def move(self, keys):
+        # Movement based on key input
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
 
     def update(self, keys):
-       
+        # Update player position based on keys
         self.move(keys)
-  
-
-   
