dbc8c5c72f75e02b8484683d803a6a24642ab0ec
Emily Scott <escott4@binghamton.edu>
2024-12-05 15:52:25 -0500

    www

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -10,27 +10,37 @@ class Controller:
         self.running = True
         self.state = "menu"
         
-        # Create platform instances as instance attributes
-        self.platform1 = World(400, 400, 150, 15)
-        self.platform2 = World(400, 300, 150, 15)
+        # Create platform instances as instance attributes (x,y,w,h)
+        self.platform1 = World(725, 525, 250, 15)
+        self.platform2 = World(0, 675, 300, 15)
+        self.platform3 = World(250,600,300,15)
+        self.platform4 = World(0,525,200,15)
+        self.platform5 = World(0,440,50,15)
+        self.platform6 = World(0,360,50,15)
+        self.platform7 = World(250,400,150,15)
+        self.platform8 = World(400,325,100,15)
+       
+        self.ground = World(0,750,1000,800)
+        self.world_bound_right = World(0,0,5,800)
+        self.world_bound_left = World(995,0,5,800)
         
 
         # Create sprite group for worlds (platforms)
         self.worlds = pygame.sprite.Group()
-        self.worlds.add(self.platform1, self.platform2)
+        self.worlds.add(self.platform1, self.platform2,self.ground,self.world_bound_right,self.world_bound_left,self.platform3,self.platform4, self.platform5,self.platform6, self.platform7,self.platform8)
 
     def mainloop(self):
       # Setup screen and clock
-      self.screen = pygame.display.set_mode((800, 600))
+      self.screen = pygame.display.set_mode((1000, 800))
       self.clock = pygame.time.Clock()
 
       # Create player instance
-      self.player = Player(375, 275)  # Starting position near the center
+      self.player = Player(0,750)  # Starting position near the center
 
       # Create sprite group for easy update and draw
       self.all_sprites = pygame.sprite.Group()
       self.all_sprites.add(self.player)
-      self.all_sprites.add(self.platform1, self.platform2)  # Add platforms to all_sprites
+      self.all_sprites.add(self.platform1, self.platform2, self.ground, self.world_bound_right,self.world_bound_left,self.platform3,self.platform4, self.platform5,self.platform6, self.platform7,self.platform8)  # Add platforms to all_sprites
 
       while self.running:
           # Handle events
--- template_final_project-master/src/player.py
+++ template_final_project-master/src/player.py
@@ -17,42 +17,60 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.5  # Force pulling the player down
         self.jump_strength = -10  # Jumping force
 
-        # Ground level (for simplicity, assume the ground is at y=550)
-        self.ground_level = 550
-        # Tracks whe user is colliding with something
-        self.on_ground = False
+        # State flags
+        self.on_ground = False  # Tracks if the player is on the ground or a platform
+        self.jump_pressed = False  # Tracks if the jump key is currently pressed
 
-    def move(self, keys):
-        # Movement based on key input
-       
+    def move(self, keys, worlds):
+        # Horizontal movement
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
+            self.check_horizontal_collision(worlds)
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
-    def the_gravity(self,worlds):
+            self.check_horizontal_collision(worlds)
+
+    def check_horizontal_collision(self, worlds):
+        # Prevent the player from moving through world pieces horizontally
+        for world in worlds:
+            if self.rect.colliderect(world.rect):
+                if self.rect.right > world.rect.left and self.rect.centerx < world.rect.centerx:
+                    self.rect.right = world.rect.left  # Prevent moving right through
+                elif self.rect.left < world.rect.right and self.rect.centerx > world.rect.centerx:
+                    self.rect.left = world.rect.right  # Prevent moving left through
+
+    def apply_gravity(self):
+        # Apply gravity
         self.y_velocity += self.gravity
         self.rect.y += self.y_velocity
-        self.on_ground = False
-       
-        ##Dont go thru ground
+        self.on_ground = False  # Assume player is in the air unless a collision occurs
 
-        if self.rect.bottom > self.ground_level:
-            self.rect.bottom = self.ground_level
-            self.y_velocity = 0  # Reset vertical velocity when on the ground
-        
+    def check_vertical_collision(self, worlds):
         for world in worlds:
-            if self.rect.colliderect(world.rect) and self.y_velocity >= 0:
-                self.rect.bottom = world.rect.top
-                self.y_velocity = 0  # Stop falling when on a platform
-                self.on_ground = True 
-                break
-    def jump(self,keys):
-        # Jump only if on the ground
-        if keys[pygame.K_SPACE] and self.y_velocity == 0:
+            if self.rect.colliderect(world.rect):
+                if self.y_velocity > 0:  # Falling
+                    self.rect.bottom = world.rect.top
+                    self.y_velocity = 0
+                    self.on_ground = True
+                elif self.y_velocity < 0:  # Jumping up
+                    self.rect.top = world.rect.bottom
+                    self.y_velocity = 0
+
+    def jump(self, keys):
+        # Jump only if on the ground and the jump key was not previously pressed
+        if keys[pygame.K_SPACE] and self.on_ground and not self.jump_pressed:
             self.y_velocity = self.jump_strength
-            self.on_ground = False
+            self.on_ground = False  # Player is now airborne
+            self.jump_pressed = True  # Mark the jump key as pressed
+
+        # Reset the jump flag when the space key is released
+        if not keys[pygame.K_SPACE]:
+            self.jump_pressed = False
+
     def update(self, keys, worlds):
         # Update player position based on keys
-        self.move(keys)
-        self.the_gravity(worlds)
+        self.move(keys, worlds)
+        self.apply_gravity()
+        self.check_vertical_collision(worlds)
+        self.check_horizontal_collision(worlds)
         self.jump(keys)
